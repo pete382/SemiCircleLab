facac8b623b6ee27c89787ced23f2be98b2e12a6
pete382 <ivan382@hotmail.com>
2024-05-13 08:00:21 -0500

    Circle Lab

---
+++ Circle.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace SemiCircleLab
+{
+    public class Circle
+    {
+        private double Radius { get; set; }
+        public double area { get; set;}
+        public double diameter { get; set; }
+        public double circumference { get; set; }
+       
+
+        //Create a constructor
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+           
+        }
+
+       
+
+        
+        
+
+        public double CalculateDiameter(double Radius) 
+        {
+            double diameter = 2 * Radius; 
+            return diameter; 
+        
+        }
+
+        public double CalculateCircumference(double Radius)
+        { 
+           double circumference = 2 * (Math.PI*Radius);
+           return circumference;
+        }
+
+        public double CalculateArea(double Radius)
+        { 
+           double area = Math.PI * ( Radius * Radius);
+           return area;
+        }
+
+      
+
+        public static implicit operator double(Circle v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Grow(double Radius)
+        {
+
+            Radius = Radius * 2;
+
+
+        }
+
+        //public double GetRadius()
+        //{ 
+
+        //    return input; 
+
+        //}
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Microsoft.VisualBasic;
+using SemiCircleLab;
+using System.Threading.Channels;
+
+double Radius = 0;
+bool isNumber = true;
+bool isGrow = false;
+
+
+Circle MyCircle = new Circle(Radius);
+Console.WriteLine("Please enter a radius.");
+
+
+    while (isNumber)
+{
+
+
+    try
+    {
+
+        Radius = double.Parse(Console.ReadLine());
+        isNumber = false;
+    }
+
+
+
+
+    catch (FormatException e)
+    { Console.WriteLine("Please enter a valid number."); }
+
+
+}
+
+
+    Console.WriteLine("Will the circle grow? Type 'y' for YES and 'n' for NO.");
+
+
+string growQuestion = Console.ReadLine().ToLower();
+
+if (growQuestion == "n")
+
+{ Radius = Radius; }
+
+else if (growQuestion == "y")
+
+{ Radius = Radius * 2; }
+
+else { }
+    //Radius = MyCircle.Grow;
+
+
+
+
+
+
+Console.WriteLine($"Diameter: {MyCircle.CalculateDiameter(Radius)}");
+Console.WriteLine($"Circumference: {MyCircle.CalculateCircumference(Radius)}");
+Console.WriteLine($"Area: {MyCircle.CalculateArea(Radius)}");
+
